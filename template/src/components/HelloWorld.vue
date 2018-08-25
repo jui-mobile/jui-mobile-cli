f1237f79e51c1156672e8490a64565b5c121d69c
zhixuanziben <714919199@qq.com>
2018-08-25 14:03:57 +0800

    init

---
+++ template/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>\{{ msg }}</h1>
+    <v-button type="primary"><a href="http://localhost:8080/">Hello JUI Mobile</a></v-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your JUI Mobile App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ template/src/main.js
@@ -0,0 +1,29 @@
+{{#if_eq build "standalone"}}
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+{{/if_eq}}
+import Vue from 'vue'
+import App from './App'
+{{#router}}
+import router from './router'
+import JUI from 'jui-mobile/dist/jui'
+import 'jui-mobile/dist/jui.css'
+{{/router}}
+
+Vue.use(JUI);
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  {{#router}}
+  router,
+  {{/router}}
+  {{#if_eq build "runtime"}}
+  render: h => h(App)
+  {{/if_eq}}
+  {{#if_eq build "standalone"}}
+  components: { App },
+  template: '<App/>'
+  {{/if_eq}}
+})
